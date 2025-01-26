1c7888a7eff785f0f5e849359dfd9d8dd878c70b
Nam9396 <nguyenthanhnam9396@gmail.com>
2025-01-26 09:55:23 +0700

    add head of page

--- src/components/ScrollHandler.jsx
+++ src/components/ScrollHandler.jsx
@@ -1,86 +1,9 @@
-'use client';
-
-// import { usePathname } from "next/navigation"
-// import { useEffect } from "react"
-
-
-// export function ScrollHandler() { 
-//   const pathname = usePathname()
-
-//   useEffect(() => {
-//     const hash = window.location.hash.replace('#', '')
-//     console.log(window.location.href)
-    
-//     if (hash) { 
-//       console.log(hash)
-//       const element = document.getElementById(hash)
-//       if (element) { 
-//         setTimeout(() => {
-//           element.scrollIntoView({ block: 'end', behavior: 'smooth' })
-//         }, 1000)
-//       }
-//     } else {
-//       console.log('hash is not available')
-//       setTimeout(() => {
-//         window.scrollTo({ top: '0', behavior: 'smooth' })
-//       }, 1000)
-//     }
-//   }, [window.location.href])
-
-//   return null
-// }
-
-
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-
-// export function useHash() {
-//   const router = useRouter()
-//   const [hash, setHash] = useState(() => {
-//     // Initialize hash only if running in the browser
-//     if (typeof window !== 'undefined') {
-//       return window.location.hash;
-//     }
-//     return ''; // Return an empty string on the server
-//   });
-
-//   useEffect(() => {
-//     const handleHashChange = () => {
-//       setHash(window.location.hash);
-//     };
-//     window.addEventListener('hashchange', handleHashChange);
-
-//     return () => {
-//       window.removeEventListener('hashchange', handleHashChange);
-//     };
-//   }, [router.asPath]);
-
-//   return hash;
-// }
+import { useEffect } from 'react';
 
 
 export function ScrollHandler() {
-  // const currentHash = useHash()
   const router = useRouter()
-  // const path = router.pathname
-  // const asPath = router.asPath
-
-  // console.log('Current hash: ', currentHash)
-
-  // console.log('pathName: ', path)
-
-  // console.log('asPath: ', asPath)
-
-  // useEffect(() => {
-  //   console.log('Current hash: ', window.location.hash)
-  //   if (window.location.hash) { 
-  //     const element = document.getElementById(window.location.hash.replace('#', ''))
-  //     console.log(element)
-  //     if (element) {
-  //       element.scrollIntoView({ behavior: 'smooth', block: 'end' });
-  //     }
-  //   }
-  // }, [asPath])
 
   useEffect(() => {
     const url = router.asPath
@@ -91,14 +14,20 @@ export function ScrollHandler() {
       const element = document.getElementById(hash)
       console.log(element)
       if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }, 2000)
+        
       }
     } else { 
-      window.scrollTo({
-        top: document.documentElement.scrollHeight, // Scroll to the bottom
-        left: 0,
-        behavior: "smooth", // Smooth scrolling (optional)
-      });
+      setTimeout(() => {
+        window.scrollTo({
+          // top: document.documentElement.scrollHeight,
+          top: 0,
+          left: 0,
+          behavior: "smooth", 
+        });
+      }, 2000)
     }
   }, [router.asPath])
 
