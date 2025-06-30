05c00b907eb956819713530427dd6e1add99a0ef
Nam9396 <nguyenthanhnam9396@gmail.com>
2025-07-01 00:38:20 +0700

    change app name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,7 +7,7 @@ export function Logo(props) {
           d="M16 8a5 5 0 0 0-5-5H5a5 5 0 0 0-5 5v13.927a1 1 0 0 0 1.623.782l3.684-2.93a4 4 0 0 1 2.49-.87H11a5 5 0 0 0 5-5V8Z"
         />      
       </svg>
-      <div className="text-black font-bold">Tự kỷ 123</div>
+      <div className="text-black font-bold">Tự kỷ Cẩm nang</div>
     </div>
   )
 }
